0f8533c8f19c644bcae77986a2e8fec0e340bc28
Kai Leahy and Kat Gifford <kat@>
2017-04-18 14:32:26 -0700

    change score to score_word

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,34 +7,34 @@ describe ("Scrabble#score") do
   end
 
   it("returns a scrabble score for a letter") do
-    expect(@scrabble.score("a")).to(eq(1))
+    expect(@scrabble.score_word("a")).to(eq(1))
   end
 
   it("returns 1 for the letters A, E, I, O, U, L, N, R, S, T") do
-    expect(@scrabble.score("a")).to(eq(1))
+    expect(@scrabble.score_word("a")).to(eq(1))
   end
 
   it("returns 2 for the letters D and G") do
-    expect(@scrabble.score("d")).to(eq(2))
+    expect(@scrabble.score_word("d")).to(eq(2))
   end
 
   it("returns 3 for the letters B, C, M, and P") do
-    expect(@scrabble.score("b")).to(eq(3))
+    expect(@scrabble.score_word("b")).to(eq(3))
   end
 
   it("returns 4 for the letters F, H, V, W, Y") do
-    expect(@scrabble.score("f")).to(eq(4))
+    expect(@scrabble.score_word("f")).to(eq(4))
   end
 
   it("returns 5 for the letter K") do
-    expect(@scrabble.score("k")).to(eq(5))
+    expect(@scrabble.score_word("k")).to(eq(5))
   end
 
   it("returns 8 for the letters J and X") do
-    expect(@scrabble.score("j")).to(eq(8))
+    expect(@scrabble.score_word("j")).to(eq(8))
   end
 
   it("returns 10 for the letters Q and Z") do
-    expect(@scrabble.score("q")).to(eq(10))
+    expect(@scrabble.score_word("q")).to(eq(10))
   end
 end
