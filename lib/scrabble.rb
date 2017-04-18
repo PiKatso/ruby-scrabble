264f56caeaf2b3a6464e3ce3078c710e5d4f9a7c
Kai Leahy and Kat Gifford <kat@>
2017-04-18 09:23:39 -0700

    add function for finding scrabble score.

---
+++ lib/scrabble.rb
@@ -0,0 +1,14 @@
+class Scrabble
+  def score(char)
+    score = {
+      ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
+      ['D', 'G'] => 2,
+      ['B', 'C', 'M', 'P'] => 3,
+      ['F', 'H', 'V', 'W', 'Y'] => 4,
+      ['K'] => 5,
+      ['J', 'X'] => 8,
+      ['Q', 'Z'] => 10
+    }
+    score.select {|key| key.include?(char.upcase)}.values[0]
+  end
+end
